c70bff4882aa213dcd218ae15302e3c00bd99b6f
zani0307 <00001076413882sp@al.educacao.sp.gov.br>
2024-05-22 10:44:47 -0300

    adicionando arquivos js e css

--- sketch.js
+++ sketch.js
@@ -1,1 +1,14 @@
+function setup(){
+    creatCanvas(600,600);
+    background("black");
+}
 
+function draw(){
+    stroke("blue");
+    fill("red");
+
+    if(mouseIsPressed){
+        rect(mouseX,mouseY,20,35);
+        
+    }
+}
